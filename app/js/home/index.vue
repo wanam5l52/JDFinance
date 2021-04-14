ca8be7d031900d246b21de76680fa478c28f66c9
wanam5l52 <zhi2791152w@126.com>
2021-04-14 15:21:37 +0800

    finish header slider

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,19 +1,22 @@
 <template lang="html">
-    <div :class="$style.home">
-        <h1>Home</h1>
-        <p>123</p>
+    <div>
+        <Heador></Heador>
+        <Slider></Slider> 
     </div>
 </template>
 
 <script>
+import Heador from "../public/header.vue";
+import Slider from "./hslider.vue";
 export default {
+  components: {
+    Heador,
+    Slider,
+  }
 }
 </script>
 
 <style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
+
+
 </style>
