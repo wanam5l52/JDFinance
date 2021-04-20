bfeb703cac6d9f6233b09701a136a85dd7333811
wanam5l52 <zhi2791152w@126.com>
2021-04-20 17:12:21 +0800

    finish ious

---
+++ app/js/ious/index.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        <Welfare></Welfare>
+        <Service></Service>
+        <Footor></Footor>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue";
+import Navbar from "../public/navbar.vue";
+import Footor from "../public/footer.vue";
+import Slider from "./islider.vue";
+import Welfare from "./welfare.vue";
+import Service from "./service.vue";
+export default {
+  components: {
+    Heador,
+    Navbar,
+    Footor,
+    Slider,
+    Welfare,
+    Service,
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
