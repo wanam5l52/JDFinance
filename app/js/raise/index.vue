80f3a746b036110fdd3808f1ac7a5a3709a52304
wanam5l52 <zhi2791152w@126.com>
2021-04-22 20:47:53 +0800

    finish raise

---
+++ app/js/raise/index.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        <Daily></Daily>
+        <In></In>
+        <More></More>
+        <Footor cname="mfooter"></Footor>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue";
+import Navbar from "../public/navbar.vue";
+import Footor from "../public/footer.vue";
+import Slider from "./rslider.vue";
+import Daily from "./daily.vue";
+import In from "./in.vue";
+import More from "./more.vue";
+export default {
+  components: {
+    Heador,
+    Navbar,
+    Footor,
+    Slider,
+    Daily,
+    In,
+    More,
+  }
+}
+</script>
+
+<style lang="scss">
+.mfooter {
+  margin-top: 0!important;
+  ul {
+    li:first-child {
+      display: none;
+    }
+  }
+}
+
+</style>
